3a9ef7ad2494a61d7600085b94bff6d6da8854cb
stm605 <31825947+stm605@users.noreply.github.com>
2018-03-17 18:19:26 +0000

    77

--- scripts/medium.sh
+++ scripts/medium.sh
@@ -16,42 +16,40 @@ echo $HANAVHOST >> /tmp/vhost.txt
 echo $SecondaryStaticIP >> /tmp/SecondaryStaticIP.txt
 echo $SecIP >> /tmp/SecIP.txt
 
-ip addr add $SecIP dev eth0 label eth0:1
-
-
 #install hana prereqs
-sudo zypper install -y glibc-2.22-51.6
-sudo zypper install -y systemd-228-142.1
-sudo zypper install -y unrar
-sudo zypper install -y krb5-client samba-winbind
-sudo zypper install -y saptune
-sudo mkdir /etc/systemd/login.conf.d
-sudo mkdir /hana
-sudo mkdir /hana/data
-sudo mkdir /hana/log
-sudo mkdir /hana/shared
-sudo mkdir /hana/backup
-sudo mkdir /usr/sap
-sudo mkdir /tmp/LaMaBits
-sudo mkdir /tmp/LaMaBits/hostagent
-sudo mkdir /tmp/LaMaBits/sapaext
+zypper install -y glibc-2.22-51.6
+zypper install -y systemd-228-142.1
+zypper install -y unrar
+zypper install -y krb5-client samba-winbind
+zypper install -y saptune
+mkdir /etc/systemd/login.conf.d
+mkdir /hana
+mkdir /hana/data
+mkdir /hana/log
+mkdir /hana/shared
+mkdir /hana/backup
+mkdir /usr/sap
+mkdir /usr/sap/$HANASID
+mkdir /tmp/LaMaBits
+mkdir /tmp/LaMaBits/hostagent
+mkdir /tmp/LaMaBits/sapaext
 
 groupadd -g 1001 sapsys
 
 # Install .NET Core and AzCopy
-sudo zypper install -y libunwind
-sudo zypper install -y libicu
+zypper install -y libunwind
+zypper install -y libicu
 curl -sSL -o dotnet.tar.gz https://go.microsoft.com/fwlink/?linkid=848824
-sudo mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
+mkdir -p /opt/dotnet && sudo tar zxf dotnet.tar.gz -C /opt/dotnet
 sudo ln -s /opt/dotnet/dotnet /usr/bin
 
 wget -O azcopy.tar.gz https://aka.ms/downloadazcopyprlinux
 tar -xf azcopy.tar.gz
 sudo ./install.sh
 
-sudo zypper se -t pattern
-sudo zypper in -t pattern sap-hana
-sudo saptune solution apply HANA
+zypper se -t pattern
+zypper in -t pattern sap-hana
+saptune solution apply HANA
 
 # step2
 echo $Uri >> /tmp/url.txt
@@ -62,72 +60,9 @@ sedcmd2="s/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=229376/g"
 cat /etc/waagent.conf | sed $sedcmd | sed $sedcmd2 > /etc/waagent.conf.new
 cp -f /etc/waagent.conf.new /etc/waagent.conf
 
-touch /home/me.txt
-
-mv /home /home.new
-mkdir /home 
-
 /usr/bin/wget --quiet $Uri/LaMaBits/SC -P /tmp/LaMaBits
 /usr/bin/wget --quiet $Uri/LaMaBits/SAPHOSTAGENT.SAR -P /tmp/LaMaBits
 /usr/bin/wget --quiet $Uri/LaMaBits/SAPACEXT.SAR -P /tmp/LaMaBits
-
-number="$(lsscsi [*] 0 0 4| cut -c2)"
-echo "logicalvols start" >> /tmp/parameter.txt
-  hanavg1lun="$(lsscsi $number 0 0 4 | grep -o '.\{9\}$')"
-  hanavg2lun="$(lsscsi $number 0 0 5 | grep -o '.\{9\}$')"
-  pvcreate hanavg $hanavg1lun $hanavg2lun
-  vgcreate hanavg $hanavg1lun $hanavg2lun
-  lvcreate -l 80%FREE -n datalv hanavg
-  lvcreate -l 20%VG -n loglv hanavg
-  mkfs.xfs /dev/hanavg/datalv
-  mkfs.xfs /dev/hanavg/loglv
-echo "logicalvols end" >> /tmp/parameter.txt
-
-
-#!/bin/bash
-echo "logicalvols2 start" >> /tmp/parameter.txt
-  sharedvglun="$(lsscsi $number 0 0 0 | grep -o '.\{9\}$')"
-  usrsapvglun="$(lsscsi $number 0 0 1 | grep -o '.\{9\}$')"
-  backupvglun1="$(lsscsi $number 0 0 2 | grep -o '.\{9\}$')"
-  backupvglun2="$(lsscsi $number 0 0 3 | grep -o '.\{9\}$')"
-  pvcreate $backupvglun1 $backupvglun2 $sharedvglun $usrsapvglun
-  vgcreate backupvg $backupvglun1 $backupvglun2
-  vgcreate sharedvg $sharedvglun
-  vgcreate usrsapvg $usrsapvglun 
-  lvcreate -l 100%FREE -n sharedlv sharedvg 
-  lvcreate -l 100%FREE -n backuplv backupvg 
-  lvcreate -l 97%VG -n usrsaplv usrsapvg
-  lvcreate -l 3%VG -n homelv usrsapvg
-  mkfs -t xfs /dev/sharedvg/sharedlv 
-  mkfs -t xfs /dev/backupvg/backuplv 
-  mkfs -t xfs /dev/usrsapvg/usrsaplv
-  mkfs -t xfs /dev/usrsapvg/homelv
-echo "logicalvols2 end" >> /tmp/parameter.txt
-
-
-#!/bin/bash
-echo "mounthanashared start" >> /tmp/parameter.txt
-mount -t xfs /dev/sharedvg/sharedlv /hana/shared
-mount -t xfs /dev/backupvg/backuplv /hana/backup 
-mount -t xfs /dev/usrsapvg/usrsaplv /usr/sap
-mount -t xfs /dev/usrsapvg/homelv /home
-mount -t xfs /dev/hanavg/datalv /hana/data
-mount -t xfs /dev/hanavg/loglv /hana/log 
-mkdir /hana/data/sapbits
-echo "mounthanashared end" >> /tmp/parameter.txt
-
-echo "write to fstab start" >> /tmp/parameter.txt
-echo "/dev/mapper/hanavg-datalv /hana/data xfs defaults 0 0" >> /etc/fstab
-echo "/dev/mapper/hanavg-loglv /hana/log xfs defaults 0 0" >> /etc/fstab
-echo "/dev/mapper/sharedvg-sharedlv /hana/shared xfs defaults 0 0" >> /etc/fstab
-echo "/dev/mapper/backupvg-backuplv /hana/backup xfs defaults 0 0" >> /etc/fstab
-echo "/dev/mapper/usrsapvg-usrsaplv /usr/sap xfs defaults 0 0" >> /etc/fstab
-echo "/dev/mapper/usrsapvg-homelv /home xfs defaults 0 0" >> /etc/fstab
-echo "write to fstab end" >> /tmp/parameter.txt
-
-mv /home.new/* /home
-
-echo "It worked" >> /home/me.txt
 
 chmod -R 777 /tmp/LaMaBits
 
@@ -156,12 +91,63 @@ cd /usr/sap/hostctrl/exe/
 chown root:sapsys sapacext
 chmod 750 sapacext
 
+/usr/sap/hostctrl/exe/sapacosprep -a ifup -i "eth0" -h $HANAVHOST -n 255.255.255.0 &> /tmp/sapacosprep.txt
+
+number="$(lsscsi [*] 0 0 4| cut -c2)"
+echo "logicalvols start" >> /tmp/parameter.txt
+  hanavg1lun="$(lsscsi $number 0 0 4 | grep -o '.\{9\}$')"
+  hanavg2lun="$(lsscsi $number 0 0 5 | grep -o '.\{9\}$')"
+  pvcreate hanavg $hanavg1lun $hanavg2lun
+  vgcreate hanavg $hanavg1lun $hanavg2lun
+  lvcreate -l 80%FREE -n datalv hanavg
+  lvcreate -l 20%VG -n loglv hanavg
+  mkfs.xfs /dev/hanavg/datalv
+  mkfs.xfs /dev/hanavg/loglv
+echo "logicalvols end" >> /tmp/parameter.txt
+
+
+#!/bin/bash
+echo "logicalvols2 start" >> /tmp/parameter.txt
+  sharedvglun="$(lsscsi $number 0 0 0 | grep -o '.\{9\}$')"
+  usrsapvglun="$(lsscsi $number 0 0 1 | grep -o '.\{9\}$')"
+  backupvglun1="$(lsscsi $number 0 0 2 | grep -o '.\{9\}$')"
+  backupvglun2="$(lsscsi $number 0 0 3 | grep -o '.\{9\}$')"
+  pvcreate $backupvglun1 $backupvglun2 $sharedvglun $usrsapvglun
+  vgcreate backupvg $backupvglun1 $backupvglun2
+  vgcreate sharedvg $sharedvglun
+  vgcreate usrsapvg $usrsapvglun 
+  lvcreate -l 100%FREE -n sharedlv sharedvg 
+  lvcreate -l 100%FREE -n backuplv backupvg 
+  lvcreate -l 100%VG -n usrsaplv usrsapvg
+  mkfs -t xfs /dev/sharedvg/sharedlv 
+  mkfs -t xfs /dev/backupvg/backuplv 
+  mkfs -t xfs /dev/usrsapvg/usrsaplv
+echo "logicalvols2 end" >> /tmp/parameter.txt
+
+
+#!/bin/bash
+echo "mounthanashared start" >> /tmp/parameter.txt
+mount -t xfs /dev/sharedvg/sharedlv /hana/shared
+mount -t xfs /dev/backupvg/backuplv /hana/backup 
+mount -t xfs /dev/usrsapvg/usrsaplv /usr/sap/$HANASID
+mount -t xfs /dev/hanavg/datalv /hana/data
+mount -t xfs /dev/hanavg/loglv /hana/log 
+mkdir /hana/data/sapbits
+echo "mounthanashared end" >> /tmp/parameter.txt
+
+echo "write to fstab start" >> /tmp/parameter.txt
+echo "/dev/mapper/hanavg-datalv /hana/data xfs defaults 0 0" >> /etc/fstab
+echo "/dev/mapper/hanavg-loglv /hana/log xfs defaults 0 0" >> /etc/fstab
+echo "/dev/mapper/sharedvg-sharedlv /hana/shared xfs defaults 0 0" >> /etc/fstab
+echo "/dev/mapper/backupvg-backuplv /hana/backup xfs defaults 0 0" >> /etc/fstab
+echo "/dev/mapper/usrsapvg-usrsaplv /usr/sap/$HANASID xfs defaults 0 0" >> /etc/fstab
+echo "write to fstab end" >> /tmp/parameter.txt
+
+
 if [ ! -d "/hana/data/sapbits" ]
  then
  mkdir "/hana/data/sapbits"
 fi
-
-
 
 #!/bin/bash
 cd /hana/data/sapbits
