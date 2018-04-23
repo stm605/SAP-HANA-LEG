5048c9940e715daec2e612a50f7bd361ca00001c
stm605 <31825947+stm605@users.noreply.github.com>
2018-04-23 18:52:43 +0000

    kk

--- scripts/lama.sh
+++ scripts/lama.sh
@@ -25,7 +25,7 @@ sudo mkdir /sapcds
 groupadd -g 1001 sapsys
 useradd -g 1001 -u 488 -s /bin/false sapadm
 useradd -g 1001 -u 1001 -s /bin/csh s42adm
-useradd -g 1001 -u 1002 -s /bin/csh s43adm
+useradd -g 1001 -u 1012 -s /bin/csh s44adm
 useradd -g 1001 -u 1010 -s /bin/csh s41adm
 useradd -g 1001 -u 1005 -s /bin/sh -M s49adm
 useradd -g 1001 -u 1006 -s /bin/sh -M s48adm
--- scripts/lamadb.sh
+++ scripts/lamadb.sh
@@ -19,11 +19,10 @@ sudo mkdir /tmp/LaMaBits/sapaext
 groupadd -g 1001 sapsys
 useradd -g 1001 -u 488 -s /bin/false sapadm
 useradd -g 1001 -u 1001 -s /bin/csh s42adm
-useradd -g 1001 -u 1002 -s /bin/csh s43adm
 useradd -g 1001 -u 1010 -s /bin/sh -d /usr/sap/S41/home s41adm
+useradd -g 1001 -u 1011 -s /bin/sh -d /usr/sap/S43/home s43adm
 useradd -g 1001 -u 1005 -s /bin/sh -M s49adm
 useradd -g 1001 -u 1006 -s /bin/sh -M s48adm
-
 
 # Install .NET Core and AzCopy
 sudo zypper install -y libunwind
